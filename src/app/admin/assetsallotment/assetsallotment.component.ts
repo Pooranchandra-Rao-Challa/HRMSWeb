83abb3efcf066bbcddd483e737a6bb7dd25ba767
sadikh <sadikh.sharieff@calibrage.in>
2023-08-08 11:15:22 +0530

    Ui Changes routing

--- src/app/admin/assetsallotment/assetsallotment.component.ts
+++ src/app/admin/assetsallotment/assetsallotment.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
-import { SelectItem } from 'primeng/api';
 import { DataView } from 'primeng/dataview';
 import { SecurityService } from 'src/app/demo/service/security.service';
-import { Employee } from 'src/app/demo/api/security';
+import { Assets, Employee } from 'src/app/demo/api/security';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ThisReceiver } from '@angular/compiler';
 
 @Component({
   selector: 'app-assetsallotment',
@@ -11,28 +12,61 @@ import { Employee } from 'src/app/demo/api/security';
   ]
 })
 export class AssetsallotmentComponent {
+  date: Date | undefined;
   color1: string = 'Bluegray';
   sortField: string = '';
   sortOrder: number = 0;
-
+  fbassets!: FormGroup;
+  submitLabel!: string;
+  dialog: boolean = false;
   visible: boolean = false;
   showDialog() {
     this.visible = true;
+
   }
 
   employees: Employee[] = [];
 
 
-  constructor(private securityService: SecurityService) { }
+  constructor(private securityService: SecurityService, private formbuilder: FormBuilder) { }
 
 
   ngOnInit() {
+    this.assetsForm();
     this.securityService.getEmployees().then((data) => (this.employees = data));
 
+  }
+  assetsForm() {
+    this.fbassets = this.formbuilder.group({
+      code: new FormControl('', [Validators.required]),
+      name: new FormControl('', [Validators.required]),
+      purchasedDate: new FormControl('', [Validators.required]),
+      modelNumber: new FormControl('', [Validators.required]),
+      manufacturer: new FormControl('', [Validators.required]),
+      serialNumber: new FormControl('', [Validators.required]),
+      warranty: new FormControl('', [Validators.required]),
+      addValue: new FormControl('', [Validators.required]),
+      description: new FormControl('', [Validators.required]),
+      status: new FormControl('', [Validators.required]),
+      isActive: new FormControl('', [Validators.required]),
+      createdAt: new FormControl('', [Validators.required]),
+      updatedAt: new FormControl('', [Validators.required]),
+      createdBy: new FormControl('', [Validators.required]),
+      updatedBy: new FormControl('', [Validators.required]),
+      assetsDetails: this.formbuilder.array([])
+    });
   }
 
   onFilter(dv: DataView, event: Event) {
     dv.filter((event.target as HTMLInputElement).value);
   }
+  onClose() {
+    this.fbassets.reset();
+  }
+  addAssetsDialog() {
+    this.dialog = true;
+    this.onClose();
+  }
+  onSubmit() { }
 
 }
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,9 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
+import { PrimengModule } from '../_shared/primeng.module';
+import { NewPasswordComponent } from './newpassword/newpassword.component';
+import { LockScreenComponent } from './lockscreen/lockscreen.component';
+import { ErrorComponent } from './error/error.component';
+import { AccessdeniedComponent } from './accessdenied/accessdenied.component';
+import { AppConfigModule } from '../layout/config/app.config.module';
 
 @NgModule({
-    declarations: [],
-    imports: [CommonModule, AuthRoutingModule]
+    declarations: [
+        NewPasswordComponent,
+        LockScreenComponent,
+        ErrorComponent,
+        AccessdeniedComponent
+    ],
+    imports: [CommonModule, AuthRoutingModule,PrimengModule,AppConfigModule]
 })
 export class AuthModule {}
