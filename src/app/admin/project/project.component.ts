08517d96dae0bb56ea3ac76c0345864d3b8ac5ca
HimaUndi <himamadhuri.u@calibrage.in>
2023-08-09 14:33:41 +0530

    Modifications in Project View

--- src/app/admin/project/project.component.ts
+++ src/app/admin/project/project.component.ts
@@ -9,59 +9,60 @@ import { SecurityService } from 'src/app/demo/service/security.service';
   templateUrl: './project.component.html',
   styles: ['']
 })
-export class ProjectComponent implements OnInit{
-employees: Employee[]= [];
-projects: ProjectDetailsDto[] = [];
-visible: boolean=false;
-fbproject!: FormGroup;
-userForm!:FormGroup;
-dialog: boolean;
-submitLabel!: string;
-constructor(private projectService:SecurityService,private formbuilder:FormBuilder){}
+export class ProjectComponent implements OnInit {
+  employees: Employee[] = [];
+  projects: ProjectDetailsDto[] = [];
+  visible: boolean = false;
+  fbproject!: FormGroup;
+  userForm!: FormGroup;
+  dialog: boolean;
+  submitLabel!: string;
+  constructor(private projectService: SecurityService, private formbuilder: FormBuilder) { }
 
-ngOnInit(){
- this.initProjects();
- this.initEmployees();
- this.fbproject = this.formbuilder.group({
-  code: new FormControl('', [Validators.required]),
-  name: new FormControl('', [Validators.required]),
-  startDate:new FormControl('',[Validators.required]),
-  description:new FormControl('',[Validators.required]),
-  managerName:new FormControl('',[Validators.required]),
-  isActive: [null],
-  companyFullName:new FormControl('',[Validators.required]),
-  gstNo:new FormControl('',[Validators.required]),
-  clientPocName:new FormControl('',[Validators.required]),
-  clientPocNo:new FormControl('',[Validators.required]),
-  address:new FormControl('',[Validators.required]),
-   id: new FormControl('', [Validators.required]),
-  empname: new FormControl('', [Validators.required]),
-  empcode: new FormControl('', [Validators.required]),
-  designation: new FormControl('', [Validators.required]),
- });
-}
+  ngOnInit() {
+    this.initProjects();
+    this.initEmployees();
+    this.fbproject = this.formbuilder.group({
+      code: new FormControl('', [Validators.required]),
+      name: new FormControl('', [Validators.required]),
+      startDate: new FormControl('', [Validators.required]),
+      description: new FormControl('', [Validators.required]),
+      managerName: new FormControl('', [Validators.required]),
+      isActive: [null],
+      companyFullName: new FormControl('', [Validators.required]),
+      gstNo: new FormControl('', [Validators.required]),
+      clientPocName: new FormControl('', [Validators.required]),
+      clientPocNo: new FormControl('', [Validators.required]),
+      address: new FormControl('', [Validators.required]),
+      id: new FormControl('', [Validators.required]),
+      empname: new FormControl('', [Validators.required]),
+      empcode: new FormControl('', [Validators.required]),
+      designation: new FormControl('', [Validators.required]),
+    });
+  }
 
 
-showDialog() {
-  this.visible = true;
-}
-onSubmit(){
+  showDialog() {
+    this.visible = true;
+  }
+  onSubmit() {
 
-}
+  }
 
-initProjects(){
-  this.projectService.getprojects().then((data: ProjectDetailsDto[]) => (this.projects = data));
-}
-initEmployees(){
-  this.projectService.getEmployees().then((data:Employee[])=> (this.employees =data));
-}
-addProjectDialog(){
-  this.dialog = true;
-  this.submitLabel = "Add Project";
-}
-editProjectDialog(){
-  this.dialog = true;
-  this.submitLabel = "Edit Project";
-}
-  
+  initProjects() {
+    this.projectService.getprojects().then((data: ProjectDetailsDto[]) => (this.projects = data));
+  }
+  initEmployees() {
+    this.projectService.getEmployees().then((data: Employee[]) => (this.employees = data));
+  }
+  addProjectDialog() {
+    this.dialog = true;
+    this.submitLabel = "Add Project";
+    this.fbproject.reset();
+  }
+  editProjectDialog() {
+    this.dialog = true;
+    this.submitLabel = "Edit Project";
+  }
+
 }
