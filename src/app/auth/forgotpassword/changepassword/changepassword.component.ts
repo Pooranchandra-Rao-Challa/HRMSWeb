08c5bb272c3fe4cacf195e19045706717eaa3a7a
SaiKarthik1006 <riya@cis10137>
2023-08-09 16:36:13 +0530

    completed logic for forgot password screen.

--- src/app/auth/forgotpassword/changepassword/changepassword.component.ts
+++ src/app/auth/forgotpassword/changepassword/changepassword.component.ts
@@ -1,58 +1,60 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
-import { ForgotUserPasswordDto } from 'src/app/demo/api/security';
+import { ForgotUserPasswordDto } from 'src/app/_models/security';
+import { SecurityService } from 'src/app/_services/security.service';
+import { ConfirmedValidator } from 'src/app/_validators/confirmValidator';
 
 @Component({
-  selector: 'app-changepassword',
-  templateUrl: './changepassword.component.html',
-  styles: [
-  ]
+    selector: 'app-changepassword',
+    templateUrl: './changepassword.component.html',
+    styles: [
+    ]
 })
 export class ChangepasswordComponent {
+    fbChangePassword!: FormGroup;
+    changePassword: ForgotUserPasswordDto = {}
 
-  fbChangePassword!: FormGroup;
-  changePassword: ForgotUserPasswordDto = {}
+    constructor(private router: Router,
+        private formbuilder: FormBuilder,
+        private securityService: SecurityService,
+        private activatedRoute: ActivatedRoute) { }
 
-  constructor(private router: Router,
-    private formbuilder: FormBuilder,
-    private activatedRoute: ActivatedRoute) { }
+    ngOnInit(): void {
+        this.changePasswordForm();
+        this.fbChangePassword.controls['userName'].setValue(this.activatedRoute.snapshot.queryParams['username']);
+    }
 
-  navigateToPrev() {
-    this.router.navigate(['auth/forgotpassword/securityquestion'])
-  }
+    changePasswordForm() {
+        this.fbChangePassword = this.formbuilder.group({
+            userName: new FormControl(''),
+            password: new FormControl('', Validators.required),
+            confirmPassword: new FormControl('', Validators.required)
+        }, {
+            validator: ConfirmedValidator('password', 'confirmPassword')
+        });
+    }
 
-  navigateToNext() {
-    this.router.navigate(['auth/forgotpassword/successmessage'])
+    get FormControls() {
+        return this.fbChangePassword.controls;
+    }
 
-    // if (this.fbChangePassword.valid) {
-    //   this.securityService.UpdateForgotPassword(this.fbChangePassword.value).subscribe(resp => {
-    //     if (resp as unknown as boolean) {
-    //       this.router.navigate(['/forgotpassword/successmessage'])
-    //     }
-    //   })
-    // }
-    // else {
-    //   this.fbChangePassword.markAllAsTouched();
-    // }
-  }
+    navigateToPrev() {
+        this.router.navigate(['auth/forgotpassword/username'], { queryParams: { username: this.changePassword.UserName } })
+    }
 
-  ngOnInit(): void {
-    this.changePasswordForm();
-    this.fbChangePassword.controls['auth/forgotpassword/userName'].setValue(this.activatedRoute.snapshot.queryParams['username']);
-  }
-
-  changePasswordForm() {
-    this.fbChangePassword = this.formbuilder.group({
-      userName: new FormControl(''),
-      password: new FormControl('', Validators.required),
-      confirmPassword: new FormControl('', Validators.required)
-    }, {
-    });
-  }
-
-  get FormControls() {
-    return this.fbChangePassword.controls;
-  }
+    navigateToNext() {
+        if (this.fbChangePassword.valid) {
+            console.log(this.fbChangePassword);
+            this.securityService.ForgotPassword(this.fbChangePassword.value).subscribe(resp => {
+                if (resp as unknown as boolean) {
+                    this.router.navigate(['auth/forgotpassword/successmessage'])
+                }
+            })
+        }
+        else {
+            this.fbChangePassword.markAllAsTouched();
+        }
+    }
 
 }
