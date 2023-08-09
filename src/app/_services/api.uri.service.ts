f5c911c52d493725a5fe3453544114c983d81a23
SaiKarthik1006 <riya@cis10137>
2023-08-09 16:31:43 +0530

    updated api and login service files.

--- src/app/_services/api.uri.service.ts
+++ src/app/_services/api.uri.service.ts
@@ -2,3 +2,5 @@
 export const LOGIN_URI = "Security/Login";
 export const REFRESH_TOKEN_URI = "Security/Refresh";
 export const REVOKE_TOKEN_URI = "Security/Revoke";
+export const USER_SECURITY_QUESTIONS_URI = "Security/UserQuestions";
+export const FORGOT_PASSWORD_URI = "Security/ForgotPassword";
