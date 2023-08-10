f3cb8439616aadb6eb9a920bfa940186796af2da
ramya5432 <122013931+ramya5432@users.noreply.github.com>
2023-08-10 11:39:32 +0530

    changes related to create security questions

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -11,6 +11,7 @@ import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { HrmsAPIInterceptor } from '../_helpers/hrms.api.interceptor';
 import { SecurityquestionsComponent } from './securityquestions/securityquestions.component';
 import { SettingsComponent } from './settings/settings.component';
+import { MessageService } from 'primeng/api';
 
 @NgModule({
     declarations: [
@@ -22,7 +23,7 @@ import { SettingsComponent } from './settings/settings.component';
         SettingsComponent
     ],
     imports: [CommonModule, AuthRoutingModule,PrimengModule,AppConfigModule],
-    providers:[{ provide: HTTP_INTERCEPTORS, useClass: HrmsAPIInterceptor, multi: true }]
+    providers:[MessageService,{ provide: HTTP_INTERCEPTORS, useClass: HrmsAPIInterceptor, multi: true }]
 
 })
 export class AuthModule {}
