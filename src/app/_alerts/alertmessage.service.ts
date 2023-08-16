722086edd4ac33a61ce2528b16838032df0f68f6
Madhuri-Minna <135100445+Madhuri-Minna@users.noreply.github.com>
2023-08-16 13:12:35 +0530

    Modifications in  Roles Screen

--- src/app/_alerts/alertmessage.service.ts
+++ src/app/_alerts/alertmessage.service.ts
@@ -75,8 +75,8 @@ export const ALERT_CODES: { [key: string]: string } = {
   'SSECP001': 'Password Updated Successfully',
   'ESECP001': 'Invalid Current Password',
 
-  'SMR001': 'Roles Add Successfully',
-  'SMR002': 'Roles Update Successfully',
+  'SMR001': 'Roles Added Successfully',
+  'SMR002': 'Roles Updated Successfully',
 
   'SMU002': 'User Update Successfully',
 
