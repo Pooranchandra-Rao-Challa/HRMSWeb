9b91347411d70e0b122464d61e78c636915ff9b4
HimaUndi <himamadhuri.u@calibrage.in>
2023-08-16 18:29:57 +0530

    Modifications in Employee Screen

--- src/app/employee/viewemployees/viewemployees.component.ts
+++ src/app/employee/viewemployees/viewemployees.component.ts
@@ -1,7 +1,25 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-import { Employee } from 'src/app/demo/api/security';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Address, Employee, familyDetailViewDto } from 'src/app/demo/api/security';
 import { SecurityService } from 'src/app/demo/service/security.service';
+
+
+export class Experience {
+  id?: number;
+  companyName?: string;
+  fromDate?: Date;
+  toDate?: Date;
+  designation?: string;
+  experienceDetails?: string;
+}
+interface States {
+  statename: string;
+  code: string;
+}
+interface General {
+  name: string;
+  code: string;
+}
 
 interface Gender {
   name: string;
@@ -49,14 +67,18 @@ export class ViewemployeesComponent {
   designation: Designation[] | undefined;
   dialog: boolean = false;
   visible: boolean = false;
-  Education :boolean =false;
+  Education: boolean = false;
+  Experience: boolean = false;
+  Family: boolean = false;
+  Address: boolean = false;
+  Documents: boolean = false;
   skillSets!: Skills[];
   personalDetails !: FormGroup;
   OfficialDetails !: FormGroup;
-  fbEducationDetails!:FormGroup;
+  fbEducationDetails!: FormGroup;
   educationDetails: any[] = [];
-  ShoweducationDetails:boolean=false;
-
+  ShoweducationDetails: boolean = false;
+  uploadedFiles: any[] = [];
   showDialog() {
     this.dialog = true;
     this.personalDetails.reset();
@@ -65,21 +87,37 @@ export class ViewemployeesComponent {
     this.visible = true;
     this.OfficialDetails.reset();
   }
-  ShowEducationDetails(){
-    this.Education= true;
+  ShowEducationDetails() {
+    this.Education = true;
+    this.fbEducationDetails.reset();
+  }
+  showExperienceDetails() {
+    this.Experience = true;
+    this.fbexperience.reset();
+  }
+  showFamilyDetails() {
+    this.Family = true;
+    this.fbfamilyDetails.reset();
+  }
+  showAddressDetails() {
+    this.Address = true;
+    this.fbAddressDetails.reset();
+  }
+  showDocumentsDetails() {
+    this.Documents = true;
   }
   constructor(private securityService: SecurityService, private formbuilder: FormBuilder) { }
-  
-  States= [
+
+  States = [
     { name: 'Andhra Pradesh', code: 'AP' },
     { name: 'Telangana', code: 'TS' }
   ];
-Courses=[
-  { name: 'SSC', code: 'SSC' },
-  { name: 'Inter', code: 'Inter' },
-  { name:'Under Graduation', code: 'UG' },
-  { name:'Post Graduation', code: 'PG' },
-]
+  Courses = [
+    { name: 'SSC', code: 'SSC' },
+    { name: 'Inter', code: 'Inter' },
+    { name: 'Under Graduation', code: 'UG' },
+    { name: 'Post Graduation', code: 'PG' },
+  ]
   ngOnInit(): void {
     this.securityService.getEmployees().then((data) => (this.employees = data));
 
@@ -144,20 +182,172 @@ Courses=[
       gradingsystem: new FormControl(''),
       cgpa: new FormControl(''),
     });
+
+    this.experienceForm();
+    this.addexperienceDetails();
+    this.relationshipStatus = [
+      { name: 'Father', code: 'father' },
+      { name: 'Mother', code: 'mother' },
+      { name: 'Spouse', code: 'spouse' },
+      { name: 'Daughter', code: 'daughter' },
+      { name: 'Son', code: 'son' },
+      { name: 'Sister', code: 'sister' }];
+    this.initFamily();
+    this.addFamilyMembers();
+    this.State = [
+      { statename: 'Andhra Pradesh', code: 'ap' },
+      { statename: 'Telangana', code: 'ts' }
+    ];
+    this.initAddress();
+
+    this.addNewAddress();
   }
-  
-  saveEducationDetails(){
+
+  saveEducationDetails() {
     if (this.fbEducationDetails.valid) {
-          const educationData = this.fbEducationDetails.value;
-          this.educationDetails.push(educationData);
-          this.clearForm();
-          this.ShoweducationDetails = true;
-        }
-      }
-      // removeEducationEntry(index: number) {
-      //   this.educationDetails.splice(index, 1);
-      // }
-        clearForm() {
-        this.fbEducationDetails.reset();
-      }
+      const educationData = this.fbEducationDetails.value;
+      this.educationDetails.push(educationData);
+      this.clearForm();
+      this.ShoweducationDetails = true;
+    }
+  }
+  clearForm() {
+    this.fbEducationDetails.reset();
+  }
+  selectedOption: string;
+  inputValue: string;
+  fbexperience!: FormGroup;
+  faexperienceDetails!: FormArray;
+  addfields: any;
+
+  ShowexperienceDetails: boolean = false;
+  toggleInputField(option: string) {
+    this.selectedOption = option;
+  }
+  addexperienceDetails() {
+    this.ShowexperienceDetails = true;
+    this.faexperienceDetails = this.fbexperience.get("experienceDetails") as FormArray
+    this.faexperienceDetails.push(this.generaterow())
+
+  }
+  experienceForm() {
+    this.addfields = []
+    this.fbexperience = this.formbuilder.group({
+      id: [],
+      companyName: new FormControl('', [Validators.required]),
+      fromDate: new FormControl('', [Validators.required]),
+      toDate: new FormControl('', [Validators.required]),
+      designation: new FormControl('', [Validators.required]),
+      experienceDetails: this.formbuilder.array([])
+    });
+  }
+  generaterow(experienceDetails: Experience = new Experience()): FormGroup {
+    return this.formbuilder.group({
+      id: [experienceDetails.id],
+      companyName: new FormControl(experienceDetails.companyName, [Validators.required]),
+      fromDate: new FormControl(experienceDetails.fromDate, [Validators.required]),
+      toDate: new FormControl(experienceDetails.toDate, [Validators.required]),
+      designation: new FormControl(experienceDetails.designation, [Validators.required]),
+    })
+  }
+  faexperienceDetail(): FormArray {
+    return this.fbexperience.get("experienceDetails") as FormArray
+  }
+  relationshipStatus: General[] | undefined;
+  fbfamilyDetails: FormGroup;
+  fafamilyDetails!: FormArray;
+  submitLabel: string;
+  faFamilyDetail(): FormArray {
+    return this.fbfamilyDetails.get("familyDetails") as FormArray
+  }
+
+  initFamily() {
+    this.fbfamilyDetails = this.formbuilder.group({
+      name: new FormControl('', [Validators.required]),
+      relationShip: new FormControl('', [Validators.required]),
+      mobileNo: new FormControl('', [Validators.required]),
+      Address: new FormControl('', [Validators.required]),
+      Nominee: new FormControl(true, [Validators.required]),
+      familyDetails: this.formbuilder.array([])
+    });
+  }
+
+  generaterow1(familyDetails: familyDetailViewDto = new familyDetailViewDto()): FormGroup {
+    return this.formbuilder.group({
+      id: new FormControl(familyDetails.id),
+      name: new FormControl(familyDetails.name, [Validators.required]),
+      relationShip: new FormControl(familyDetails.relationShip, [Validators.required]),
+      mobileNo: new FormControl(familyDetails.mobileNo, [Validators.required]),
+      Address: new FormControl(familyDetails.Address, [Validators.required]),
+    });
+  }
+
+  addFamilyMembers() {
+    this.fafamilyDetails = this.fbfamilyDetails.get("familyDetails") as FormArray
+    this.fafamilyDetails.push(this.generaterow1())
+
+  }
+  State: States[] | undefined;
+  fbAddressDetails: FormGroup;
+  faAddressDetails!: FormArray;
+  showAddressDetailss: boolean = false;
+  faAddressDetail(): FormArray {
+    return this.fbAddressDetails.get("addressDetails") as FormArray
+  }
+
+  initAddress() {
+    this.fbAddressDetails = this.formbuilder.group({
+      Id: [''],
+      AddressLine1: new FormControl('', [Validators.required]),
+      AddressLine2: new FormControl('', [Validators.required]),
+      Landmark: new FormControl('', [Validators.required]),
+      ZIPCode: new FormControl('', [Validators.required]),
+      City: new FormControl('', [Validators.required]),
+      State: new FormControl('', [Validators.required]),
+      Country: new FormControl('', [Validators.required]),
+      IsActive: new FormControl('', [Validators.required]),
+      CreatedAt: new FormControl('', [Validators.required]),
+      UpdatedAt: new FormControl('', [Validators.required]),
+      CreatedBy: new FormControl('', [Validators.required]),
+      UpdatedBy: new FormControl('', [Validators.required]),
+      addressDetails: this.formbuilder.array([])
+    });
+  }
+
+  generaterow2(addressDetails: Address = new Address()): FormGroup {
+    return this.formbuilder.group({
+      Id: new FormControl(addressDetails.Id),
+      AddressLine1: new FormControl(addressDetails.AddressLine1, [Validators.required]),
+      AddressLine2: new FormControl(addressDetails.AddressLine2, [Validators.required]),
+      Landmark: new FormControl(addressDetails.Landmark, [Validators.required]),
+      ZIPCode: new FormControl(addressDetails.ZIPCode, [Validators.required]),
+      City: new FormControl(addressDetails.City, [Validators.required]),
+      State: new FormControl(addressDetails.State, [Validators.required]),
+      Country: new FormControl(addressDetails.Country, [Validators.required]),
+      IsActive: new FormControl(addressDetails.IsActive, [Validators.required]),
+      CreatedAt: new FormControl(addressDetails.CreatedAt, [Validators.required]),
+      UpdatedAt: new FormControl(addressDetails.UpdatedAt, [Validators.required]),
+      CreatedBy: new FormControl(addressDetails.CreatedBy, [Validators.required]),
+      UpdatedBy: new FormControl(addressDetails.UpdatedBy, [Validators.required]),
+    });
+  }
+
+  addNewAddress() {
+    this.showAddressDetailss = true;
+    this.faAddressDetails = this.fbAddressDetails.get("addressDetails") as FormArray
+    this.faAddressDetails.push(this.generaterow2())
+
+  }
+
+  onUpload(event: any) {
+    for (const file of event.files) {
+      this.uploadedFiles.push(file);
+    }
+
+    // this.messageService.add({ severity: 'info', summary: 'Success', detail: 'File Uploaded' });
+  }
+
+  onBasicUpload() {
+    // this.messageService.add({ severity: 'info', summary: 'Success', detail: 'File Uploaded with Basic Mode' });
+  }
 }
