72045d603832001df8c860eb58099b173a40523f
HimaUndi <himamadhuri.u@calibrage.in>
2023-08-16 17:45:37 +0530

    Modifications in Employee Details

--- src/app/employee/viewemployees/viewemployees.component.ts
+++ src/app/employee/viewemployees/viewemployees.component.ts
@@ -48,19 +48,38 @@ export class ViewemployeesComponent {
   status: Status[] | undefined;
   designation: Designation[] | undefined;
   dialog: boolean = false;
-  visible:boolean = false;
+  visible: boolean = false;
+  Education :boolean =false;
   skillSets!: Skills[];
-  personalDetails !:FormGroup;
-  OfficialDetails !:FormGroup;
+  personalDetails !: FormGroup;
+  OfficialDetails !: FormGroup;
+  fbEducationDetails!:FormGroup;
+  educationDetails: any[] = [];
+  ShoweducationDetails:boolean=false;
+
   showDialog() {
     this.dialog = true;
-this.personalDetails.reset();
+    this.personalDetails.reset();
   }
-Dialog(){
-  this.visible =true;
-  this.OfficialDetails.reset();
-}
-  constructor(private securityService: SecurityService,private formbuilder:FormBuilder) { }
+  Dialog() {
+    this.visible = true;
+    this.OfficialDetails.reset();
+  }
+  ShowEducationDetails(){
+    this.Education= true;
+  }
+  constructor(private securityService: SecurityService, private formbuilder: FormBuilder) { }
+  
+  States= [
+    { name: 'Andhra Pradesh', code: 'AP' },
+    { name: 'Telangana', code: 'TS' }
+  ];
+Courses=[
+  { name: 'SSC', code: 'SSC' },
+  { name: 'Inter', code: 'Inter' },
+  { name:'Under Graduation', code: 'UG' },
+  { name:'Post Graduation', code: 'PG' },
+]
   ngOnInit(): void {
     this.securityService.getEmployees().then((data) => (this.employees = data));
 
@@ -88,32 +107,57 @@ Dialog(){
       { name: 'MS Sql', code: 'MS' },
       { name: 'React', code: 'RE' },
       { name: 'Python', code: 'PY' }
-  ];
-  this.personalDetails = this.formbuilder.group({
-    Id: new FormControl('', [Validators.required]),
-    Name: new FormControl('', [Validators.required]),
-    Gender: new FormControl('', [Validators.required]),
-    BloodGroup: new FormControl('', [Validators.required]),
-    MobileNumber : new FormControl('', [Validators.required]),
-    AltMobileNumber : new FormControl('', [Validators.required]),
-    DOB: new FormControl('', [Validators.required]),
-    CertificateDOB: new FormControl('', [Validators.required]),
-    MaritalStatus: new FormControl('', [Validators.required]),
-    EmailID: new FormControl('', [Validators.required]),
-    CurrentAddress: new FormControl('', [Validators.required]),
-    PermanentAddress: new FormControl('', [Validators.required]),
-    SkillSet: new FormControl('', [Validators.required]),
-  });
-  this.OfficialDetails= this.formbuilder.group({
-    Id: new FormControl('', [Validators.required]),
-    Shift: new FormControl('', [Validators.required]),
-    OfficeEmailID: new FormControl('', [Validators.required]),
-    JoiningDate: new FormControl('', [Validators.required]),
-    Designation : new FormControl('', [Validators.required]),
-    ReportedTo : new FormControl('', [Validators.required]),
-    ProjectName: new FormControl('', [Validators.required]),
-    PFEligible: new FormControl('', [Validators.required]),
-    ESIEligible: new FormControl('', [Validators.required])
-  });
+    ];
+    this.personalDetails = this.formbuilder.group({
+      Id: new FormControl('', [Validators.required]),
+      Name: new FormControl('', [Validators.required]),
+      Gender: new FormControl('', [Validators.required]),
+      BloodGroup: new FormControl('', [Validators.required]),
+      MobileNumber: new FormControl('', [Validators.required]),
+      AltMobileNumber: new FormControl('', [Validators.required]),
+      DOB: new FormControl('', [Validators.required]),
+      CertificateDOB: new FormControl('', [Validators.required]),
+      MaritalStatus: new FormControl('', [Validators.required]),
+      EmailID: new FormControl('', [Validators.required]),
+      CurrentAddress: new FormControl('', [Validators.required]),
+      PermanentAddress: new FormControl('', [Validators.required]),
+      SkillSet: new FormControl('', [Validators.required]),
+    });
+    this.OfficialDetails = this.formbuilder.group({
+      Id: new FormControl('', [Validators.required]),
+      Shift: new FormControl('', [Validators.required]),
+      OfficeEmailID: new FormControl('', [Validators.required]),
+      JoiningDate: new FormControl('', [Validators.required]),
+      Designation: new FormControl('', [Validators.required]),
+      ReportedTo: new FormControl('', [Validators.required]),
+      ProjectName: new FormControl('', [Validators.required]),
+      PFEligible: new FormControl('', [Validators.required]),
+      ESIEligible: new FormControl('', [Validators.required])
+    });
+    this.fbEducationDetails = this.formbuilder.group({
+      course: new FormControl(''),
+      state: new FormControl(''),
+      school: new FormControl(''),
+      board: new FormControl(''),
+      stream: new FormControl(''),
+      yearofpass: new FormControl(''),
+      gradingsystem: new FormControl(''),
+      cgpa: new FormControl(''),
+    });
   }
+  
+  saveEducationDetails(){
+    if (this.fbEducationDetails.valid) {
+          const educationData = this.fbEducationDetails.value;
+          this.educationDetails.push(educationData);
+          this.clearForm();
+          this.ShoweducationDetails = true;
+        }
+      }
+      // removeEducationEntry(index: number) {
+      //   this.educationDetails.splice(index, 1);
+      // }
+        clearForm() {
+        this.fbEducationDetails.reset();
+      }
 }
