827447d812ac0e160a7412aaf2cf45d50303fea8
pcraochallas@calibrage.in <pcraochallas@calibrage.in>
2023-08-17 18:59:22 +0530

    cleaned the code for security qustions

--- src/app/auth/securityquestions/securityquestions.component.ts
+++ src/app/auth/securityquestions/securityquestions.component.ts
@@ -1,161 +1,155 @@
+import { BehaviorSubject, Subject } from 'rxjs';
 import { Component, ViewChild } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { MessageService } from 'primeng/api';
 import { CreateUserQuestionDto, SecureQuestionDto } from 'src/app/_models/security';
 import { JwtService } from 'src/app/_services/jwt.service';
 import { SecurityService } from 'src/app/_services/security.service';
-import jwtdecode from 'jwt-decode';
 import { Table } from 'primeng/table';
 import { Router } from '@angular/router';
 import { AlertmessageService, ALERT_CODES } from 'src/app/_alerts/alertmessage.service';
 
 export interface IHeader {
-  field: string;
-  header: string;
-  label: string;
+    field: string;
+    header: string;
+    label: string;
 }
 
 export interface SecurQuestion {
-  code: number,
-  name: string
+    code: number,
+    name: string
 }
 
 export class SecurityDto {
-  id?: number;
-  SecurityQuestions?: string;
-  Answer?: string;
+    id?: number;
+    SecurityQuestions?: string;
+    Answer?: string;
 }
 @Component({
-  selector: 'app-securityquestions',
-  templateUrl: './securityquestions.component.html',
-  styles: [
-  ]
+    selector: 'app-securityquestions',
+    templateUrl: './securityquestions.component.html',
+    styles: [
+    ]
 })
 export class SecurityquestionsComponent {
-  createUserQuestions: CreateUserQuestionDto[] = [];
-  getSecureQuestions: SecureQuestionDto[] = [];
-  allSecureQuestions: SecureQuestionDto[] = [];
-  securityquestions: SecurQuestion[];
-  selectedQuestion!: SecurQuestion;
-  securityDto: SecurityDto[] = [];
-  security!: SecurityDto;
-  securityDialog: boolean = false;
-  submitted: boolean = true;
-  qstnSubmitLabel: String = "Add";
+    //createUserQuestions: CreateUserQuestionDto[] = [];
+    allSecureQuestions: SecureQuestionDto[] = [];
+    secureQuestions: BehaviorSubject<SecureQuestionDto[]> = new BehaviorSubject([]);
+    securityDto: SecurityDto[] = [];
+    security: SecurityDto = {};
+    oldSecurity: SecurityDto = {};
+    securityDialog: boolean = false;
+    submitted: boolean = true;
+    qstnSubmitLabel: String = "Add";
 
-  constructor(
-    private formbuilder: FormBuilder,
-    private securityService: SecurityService,
-    private alertMessage: AlertmessageService,
-    private jwtService: JwtService,
-    private router: Router,) {}
-  headers: IHeader[] = [
-    { field: 'SecurityQuestions', header: 'SecurityQuestions', label: 'Security Questions' },
-    { field: 'Answer', header: 'Answer', label: 'Answer' },
-  ];
+    constructor(
+        private formbuilder: FormBuilder,
+        private securityService: SecurityService,
+        private alertMessage: AlertmessageService,
+        private jwtService: JwtService,
+        private router: Router,) { }
+    headers: IHeader[] = [
+        { field: 'SecurityQuestions', header: 'SecurityQuestions', label: 'Security Questions' },
+        { field: 'Answer', header: 'Answer', label: 'Answer' },
+    ];
 
-  openNew() {
-    this.security = {};
-    this.submitted = false;
-    this.qstnSubmitLabel = "Add";
-    this.securityDialog = true;
-  }
+    openNew() {
+        this.security = {};
+        this.submitted = false;
+        this.qstnSubmitLabel = "Add";
+        this.securityDialog = true;
+    }
 
-  initGetSecureQuestions() {
-    this.securityService.GetSecureQuestions().subscribe((resp) => {
-      this.getSecureQuestions = resp as unknown as SecureQuestionDto[];
-      this.allSecureQuestions = [...this.getSecureQuestions];
-    });
-  }
+    initGetSecureQuestions() {
+        this.securityService.GetSecureQuestions().subscribe((resp) => {
+            this.allSecureQuestions = resp as unknown as SecureQuestionDto[];
+            this.secureQuestions.next(this.allSecureQuestions);
+            console.log(this.allSecureQuestions);
 
-  ngOnInit(): void {
-    this.initGetSecureQuestions();
-  }
+        });
+    }
 
-  editSecurity(security: SecurityDto) {
-    this.security = security;
-    this.qstnSubmitLabel = "Update";
-    this.securityDialog = true;
-    this.getSecureQuestions.push({
-      questionId: this.security.id,
-      question: this.security.SecurityQuestions
-    });
-  }
+    ngOnInit(): void {
+        this.initGetSecureQuestions();
+    }
 
-  deleteSecurity(question: String) {
-    this.securityDto.splice(this.securityDto.findIndex(item => item.SecurityQuestions === question), 1);
-    this.securityDto = [...this.securityDto];
-  }
+    editSecurity(s: SecurityDto) {
+        Object.assign(this.security, s);
+        Object.assign(this.oldSecurity, s);
+        this.qstnSubmitLabel = "Update";
+        this.securityDialog = true;
+        this.resetSecureQuestions(this.security);
+    }
 
-  hideDialog() {
-    this.securityDialog = false;
-    this.submitted = false;
-  }
+    deleteSecurity(question: String) {
+        this.securityDto.splice(this.securityDto.findIndex(item => item.SecurityQuestions === question), 1);
+        this.resetSecureQuestions();
+    }
 
-  onChange(event: any) {
-    this.security.id = this.getSecureQuestions[this.getSecureQuestions.findIndex(item => item.question === event.value)].questionId;
-    this.getSecureQuestions.splice(this.getSecureQuestions.findIndex(item => item.question === event.value), 1);
-  }
+    resetSecureQuestions(security: SecurityDto = {}) {
+        let test = this.allSecureQuestions.filter((value => this.securityDto.findIndex(question => question.SecurityQuestions === value.question) == -1 || value.question === security.SecurityQuestions));
+        this.secureQuestions.next(test);
+    }
 
-  saveSecurity() {
-    this.submitted = true;
-    if (this.security.Answer?.trim()) {
-      if (this.security.id) {
-        const index = this.findIndexById(this.security.id);
-        if (index >= 0) {
-          this.securityDto[index] = this.security;
-          this.alertMessage.displayAlertMessage(ALERT_CODES["SSESQ001"]);
-        } else {
-          this.securityDto.push(this.security);
-          this.alertMessage.displayAlertMessage(ALERT_CODES["SSESQ003"]);
+    hideDialog() {
+        this.securityDialog = false;
+        this.submitted = false;
+    }
+
+    onChange(event: any) {
+        this.security.id = this.allSecureQuestions[this.allSecureQuestions.findIndex(item => item.question === event.value)].questionId;
+    }
+
+    clearSelection() {
+        this.securityDialog = false;
+        this.security = {};
+        this.oldSecurity = {};
+        this.resetSecureQuestions();
+    }
+
+    saveSecurity() {
+        this.submitted = true;
+        if (this.security.Answer?.trim() && this.security.id) {
+            let selectedIndex = this.securityDto.findIndex(value => value.SecurityQuestions == this.oldSecurity.SecurityQuestions);
+            if (selectedIndex == -1) {
+                this.securityDto.push(this.security);
+            } else {
+                this.securityDto[selectedIndex] = this.security;
+            }
+            this.clearSelection();
         }
-      }
-      this.securityDto = [...this.securityDto];
-      this.securityDialog = false;
-      this.security = {};
     }
-  }
 
-  findIndexById(id: number): number {
-    let index = -1;
-    for (let i = 0; i < this.securityDto.length; i++) {
-      if (this.securityDto[i].id === id) {
-        index = i;
-        break;
-      }
-    }
-    return index;
-  }
 
-  onSubmit() {
-    if (this.securityDto.length >= 2) {
-      const jwtToken = jwtdecode(this.jwtService.JWTToken) as unknown as any;
-      const username = jwtToken.GivenName;
-      const userId = jwtToken.Id;
-      const createUserQuestions: CreateUserQuestionDto[] = this.securityDto.map(security => {
-        return {
-          question: security.SecurityQuestions,
-          answer: security.Answer,
-          username: username,
-          userId: userId,
-          questionId: security.id,
-        };
-      });
-      this.securityService
-        .CreateSecurityQuestions(createUserQuestions)
-        .subscribe((resp) => {
-          if (resp) {
-            this.createUserQuestions = resp as unknown as CreateUserQuestionDto[];
-            this.alertMessage.displayAlertMessage(ALERT_CODES["SSESQ001"]);
-            this.securityDto = [];
-            this.router.navigate(['./dashboard/admin']);
-          }
-          else {
-            this.alertMessage.displayErrorMessage(ALERT_CODES["SSESQ002"]);
-          }
-        })
+    onSubmit() {
+
+        const username = this.jwtService.GivenName;
+        const userId = this.jwtService.UserId;
+        const createUserQuestions: CreateUserQuestionDto[] = this.securityDto.map(security => {
+            return {
+                question: security.SecurityQuestions,
+                answer: security.Answer,
+                username: username,
+                userId: userId,
+                questionId: security.id,
+            };
+        });
+        this.securityService
+            .CreateSecurityQuestions(createUserQuestions)
+            .subscribe((resp) => {
+                if (resp) {
+                    //this.createUserQuestions = resp as unknown as CreateUserQuestionDto[];
+                    this.alertMessage.displayAlertMessage(ALERT_CODES["SSESQ001"]);
+                    this.securityDto = [];
+                    this.router.navigate(['./dashboard/admin']);
+                }
+                else {
+                    this.alertMessage.displayErrorMessage(ALERT_CODES["SSESQ002"]);
+                }
+            })
+
+
     }
-  }
 }
 
 
