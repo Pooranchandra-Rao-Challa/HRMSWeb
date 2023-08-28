5d68a0a3a028b7214c7b36f7a4e2cd8d3af74f5a
SaiKarthik1006 <saikarthik@calibrage.in>
2023-08-28 10:07:34 +0530

    displaying the assets dropdown.

--- src/app/_services/admin.service.ts
+++ src/app/_services/admin.service.ts
@@ -1,49 +1,63 @@
 import { Injectable } from '@angular/core';
 import { AssetsDto, AssetsViewDto, HolidayDto, HolidaysViewDto, LookupDetailViewDto, LookUpHeaderDto, LookupViewDto } from '../_models/admin';
+import { AssetsByAssetTypeIdViewDto } from '../_models/admin/assetsallotment';
 import { ApiHttpService } from './api.http.service';
-import {  CREATE_ASSETS_URI, CREATE_HOLIDAY_URI, CREATE_LOOKUP_URI, GET_ASSETS_URI, GET_HOLIDAY_URI, GET_LOOKUP_DETAILS_URI, GET_LOOKUP_URI, UPDATE_ASSETS_URI, UPDATE_LOOKUP_URI } from './api.uri.service';
+import {
+    CREATE_ASSETS_URI, CREATE_HOLIDAY_URI, CREATE_LOOKUP_URI, GET_ASSETS_BY_ASSETTYPE_URI, GET_ASSETS_URI, GET_HOLIDAY_URI,
+    GET_LOOKUP_DETAILS_URI, GET_LOOKUP_URI, UPDATE_ASSETS_URI, UPDATE_LOOKUP_URI, CREATE_ASSET_ALLOTMENT_URI
+} from './api.uri.service';
 // import { CREATE_LOOKUP_URI } from './api.uri.service';
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class AdminService extends ApiHttpService {
-  // lookup
-  public GetLookUp(isbool) {
-    return this.get<LookupViewDto[]>(GET_LOOKUP_URI+'/' + isbool);
-  }
-  public CreateLookUp(lookup: LookUpHeaderDto) {
-    return this.post<LookUpHeaderDto>(CREATE_LOOKUP_URI, lookup );
-  }
-  public UpdateLookUp(lookup: LookUpHeaderDto) {
-    return this.post<LookUpHeaderDto>(UPDATE_LOOKUP_URI, lookup);
-  }
-  public GetlookupDetails(lookupId: number) {
-    return this.getWithId<LookupDetailViewDto[]>(GET_LOOKUP_DETAILS_URI,lookupId);
-  }
+    // lookup
+    public GetLookUp(isbool) {
+        return this.get<LookupViewDto[]>(GET_LOOKUP_URI + '/' + isbool);
+    }
+    public CreateLookUp(lookup: LookUpHeaderDto) {
+        return this.post<LookUpHeaderDto>(CREATE_LOOKUP_URI, lookup);
+    }
+    public UpdateLookUp(lookup: LookUpHeaderDto) {
+        return this.post<LookUpHeaderDto>(UPDATE_LOOKUP_URI, lookup);
+    }
+    public GetlookupDetails(lookupId: number) {
+        return this.getWithId<LookupDetailViewDto[]>(GET_LOOKUP_DETAILS_URI, lookupId);
+    }
 
-  public GetHolidays(year: string) {
-    const url = `${GET_HOLIDAY_URI}${year}`;
-    return this.get<HolidaysViewDto[]>(url);
-  }
+    public GetHolidays(year: string) {
+        const url = `${GET_HOLIDAY_URI}${year}`;
+        return this.get<HolidaysViewDto[]>(url);
+    }
 
-  public CreateHoliday(holidayDto:HolidayDto[]) {
-    return this.post<HolidayDto>(CREATE_HOLIDAY_URI, holidayDto);
-  }
-  public UpdateHoliday(holiday:any) { //HolidayDto 
-   // return this.post<HolidayDto>(UPDATE_Holiday_URI, holiday);
+    public CreateHoliday(holidayDto: HolidayDto[]) {
+        return this.post<HolidayDto>(CREATE_HOLIDAY_URI, holidayDto);
+    }
+    public UpdateHoliday(holiday: any) { //HolidayDto
+        // return this.post<HolidayDto>(UPDATE_Holiday_URI, holiday);
 
-  }
+    }
 
-  public GetAssets(){
-    return this.get<AssetsViewDto[]>(GET_ASSETS_URI);
-}
+    public GetAssets() {
+        return this.get<AssetsViewDto[]>(GET_ASSETS_URI);
+    }
 
-public CreateAssets(assets: AssetsDto) {
-  return this.post<AssetsDto>(CREATE_ASSETS_URI, assets ,{ responseType: 'text' });
-}
+    public CreateAssets(assets: AssetsDto) {
+        return this.post<AssetsDto>(CREATE_ASSETS_URI, assets, { responseType: 'text' });
+    }
 
-public UpdateAssets(assets: AssetsDto) {
-  return this.post<AssetsDto>(UPDATE_ASSETS_URI, assets);
-}
- 
+    public UpdateAssets(assets: AssetsDto) {
+        return this.post<AssetsDto>(UPDATE_ASSETS_URI, assets);
+    }
+
+    // Assest Allotment
+
+    public GetAssetsByAssetType(assetTypeId: number) {
+        return this.getWithId<AssetsByAssetTypeIdViewDto[]>(GET_ASSETS_BY_ASSETTYPE_URI, assetTypeId);
+    }
+
+    public CreateAssetAllotment(assetAllotment: any) {
+        return this.post<any>(CREATE_ASSET_ALLOTMENT_URI, assetAllotment);
+    }
+
 }
