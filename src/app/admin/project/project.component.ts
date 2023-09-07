21891e971725018b7dac0e76e111d7b468de0de6
sadikhCLS <sadikh.sharieff@calibrage.in>
2023-09-07 18:55:57 +0530

    UI Changes for hirarchi

--- src/app/admin/project/project.component.ts
+++ src/app/admin/project/project.component.ts
@@ -110,7 +110,7 @@ export class ProjectComponent implements OnInit {
     if (project != null) {
       console.log(project)
       this.addFlag = false;
-      this.submitLabel = "Update Project";
+      this.submitLabel = "Update Project Details";
       this.fbproject.patchValue({
         clientId: project.clientId,
         projectId: project.projectId,
@@ -269,7 +269,7 @@ export class ProjectComponent implements OnInit {
     return projects.map((project) => ({
       type: 'person',
       styleClass: 'bg-orange-300 text-white',
-      expanded: true,
+      expanded: false,
       data: {
         image: project.logo,
         name: project.name,
@@ -277,6 +277,13 @@ export class ProjectComponent implements OnInit {
       },
       children: [
         { label: 'Sadikh', styleClass: 'bg-green-300 text-white', },
+        { label: 'Arun', styleClass: 'bg-green-300 text-white', },
+        { label: 'Arun', styleClass: 'bg-green-300 text-white', },
+        { label: 'Arun', styleClass: 'bg-green-300 text-white', },
+        { label: 'Arun', styleClass: 'bg-green-300 text-white', },
+        { label: 'Arun', styleClass: 'bg-green-300 text-white', },
+        { label: 'Arun', styleClass: 'bg-green-300 text-white', },
+        { label: 'Arun', styleClass: 'bg-green-300 text-white', },
         { label: 'Arun', styleClass: 'bg-green-300 text-white', },
       ], // Assuming 'name' is the project name property
     }));
