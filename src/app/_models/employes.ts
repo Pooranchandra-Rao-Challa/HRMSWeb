80a6d951bea9a2f0f16dbc23d6a005af2e26f595
manikanta92-eng <manikanta.p@calibrage.in>
2023-09-12 12:11:09 +0530

    chanhes in Employe Screen

---
+++ src/app/_models/employes.ts
@@ -0,0 +1,34 @@
+export class EmployeesViewDto {
+	employeeId?: number
+	code?: string
+	employeeName?: string
+	gender?: string
+	bloodGroupId?: number
+	bloodGroup?: string
+	mobileNumber?: string
+	alternateMobileNumber?: string
+	originalDOB?: string
+	certificateDOB?: string
+	maritalStatus?: string
+	emailId?: string
+	photo?: any
+	signDate?: string
+	addresses?: any
+	bankDetails?: any
+	uploadedDocuments?: any
+	employeeInceptionDetailId?: number
+	timeIn?: string
+	timeOut?: string
+	officeEmailId?: any
+	dateofJoin?: string
+	isPFEligible?: boolean
+	isESIEligible?: boolean
+	reportingToId?: number
+	reportingTo?: any
+	roleName?: any
+	isActive?: boolean
+	createdAt?: string
+	createdBy?: string
+	updatedAt?: string
+	updatedBy?: string
+}
