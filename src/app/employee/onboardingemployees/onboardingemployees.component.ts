bbcdbef0d175c98ed01a8dfda3f71517c6b36c31
manikanta92-eng <manikanta.p@calibrage.in>
2023-09-12 15:54:46 +0530

    update in onboard display employees

--- src/app/employee/onboardingemployees/onboardingemployees.component.ts
+++ src/app/employee/onboardingemployees/onboardingemployees.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { MenuItem, SelectItem } from 'primeng/api';
 import { DataView } from 'primeng/dataview';
+import { Table } from 'primeng/table/public_api';
 import { MEDIUM_DATE } from 'src/app/_helpers/date.formate.pipe';
+import { ITableHeader } from 'src/app/_models/common';
 import { EmployeesViewDto } from 'src/app/_models/employes';
 import { EmployeeService } from 'src/app/_services/employee.service';
 import { Product } from 'src/app/demo/api/product';
@@ -21,6 +23,19 @@ export class OnboardingemployeesComponent {
   visible: boolean = false;
   newEmployeeSteps: MenuItem[];
   mediumDate: string = MEDIUM_DATE
+  @ViewChild('filter') filter!: ElementRef;
+
+  globalFilterFields: string[] = ['employeeName', 'code', 'designation', 'officeEmailId', 'mobileNumber'];
+  headers: ITableHeader[] = [
+    { field: 'employeeName', header: 'employeeName', label: 'Employee Name' }, 
+    { field: 'gender', header: 'gender', label: 'Gender' },
+    { field: 'code', header: 'code', label: 'Employee Code' },
+    { field: 'employeeRoleName', header: 'employeeRoleName', label: 'Designation' },
+    { field: 'officeEmailId', header: 'officeEmailId', label: 'Email' },
+    { field: 'mobileNumber', header: 'mobileNumber', label: 'Phone No' },
+    { field: 'dateofJoin', header: 'dateofJoin', label: 'Date of Join' }, 
+    
+  ];
 
   showDialog() {
     this.router.navigate(['basicdetails'], { relativeTo: this.route })
@@ -93,6 +108,9 @@ export class OnboardingemployeesComponent {
       this.sortField = value;
     }
   }
+
+
+
   initEmployees() {
     // Fetch only records where IsEnrolled is true
     const isEnrolled = false;
@@ -101,7 +119,14 @@ export class OnboardingemployeesComponent {
       console.log(this.employees)
     });
   }
+  onGlobalFilter(table: Table, event: Event) {
+    table.filterGlobal((event.target as HTMLInputElement).value, 'contains');
+  }
 
+  clear(table: Table) {
+    table.clear();
+    this.filter.nativeElement.value = '';
+  }
   onFilter(dv: DataView, event: Event) {
     dv.filter((event.target as HTMLInputElement).value);
   }
