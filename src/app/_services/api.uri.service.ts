9387e8219c05b3e19a5e32de5a4d506c4a97d297
SaiKarthik1006 <saikarthik@calibrage.in>
2023-09-12 14:42:36 +0530

    updated service and models with EmployeesForAllottedAssets.

--- src/app/_services/api.uri.service.ts
+++ src/app/_services/api.uri.service.ts
@@ -47,3 +47,4 @@ export const UPDATE_PROJECT_URI="Admin/UpdateProjectWithDetails";
 export const  GET_CLIENTNAMES_URI="Admin/GetClientNames";
 export const GET_CLIENT_DETAILS="Admin/GetClientDetails";
 export const GET_EMPLOYEES="Employee/GetEmployeeDropdown";
+export const EMPLOYEES_FOR_ALLOTTED_ASSETS_URI = "Admin/GetAssetsForEmployees";
