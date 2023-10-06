aa8663f9483aa855570136b8556f6142b855b929
HimaUndi <himamadhuri.u@calibrage.in>
2023-10-06 18:36:05 +0530

    Alert Messages and Removing unnecessary  code in Leaves

--- src/app/_dialogs/leave.dialog/leave.dialog.component.ts
+++ src/app/_dialogs/leave.dialog/leave.dialog.component.ts
@@ -4,12 +4,12 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 import { DynamicDialogRef } from 'primeng/dynamicdialog';
 import { Observable } from 'rxjs';
 import { AlertmessageService, ALERT_CODES } from 'src/app/_alerts/alertmessage.service';
+import { FORMAT_DATE } from 'src/app/_helpers/date.formate.pipe';
 import { EmployeesList, LookupViewDto } from 'src/app/_models/admin';
 import { MaxLength } from 'src/app/_models/common';
 import { EmployeeLeaveDto } from 'src/app/_models/employes';
 import { AdminService } from 'src/app/_services/admin.service';
 import { EmployeeService } from 'src/app/_services/employee.service';
-import { JwtService } from 'src/app/_services/jwt.service';
 import { LookupService } from 'src/app/_services/lookup.service';
 
 @Component({
@@ -20,6 +20,7 @@ export class LeaveDialogComponent {
   fbLeave!: FormGroup;
   employees: EmployeesList[] = [];
   leaveType: LookupViewDto[] = [];
+  leaves: EmployeeLeaveDto[] = [];
   filteredLeaveTypes: LookupViewDto[] = [];
   maxLength: MaxLength = new MaxLength();
   filterCriteria: string[] = ['PT', 'AT'];
@@ -29,7 +30,7 @@ export class LeaveDialogComponent {
     private lookupService: LookupService,
     private employeeService: EmployeeService,
     public ref: DynamicDialogRef,
-    public alertMessage:AlertmessageService) { }
+    public alertMessage: AlertmessageService) { }
 
   ngOnInit(): void {
     this.getEmployees();
@@ -49,6 +50,7 @@ export class LeaveDialogComponent {
       this.filteredLeaveTypes = this.leaveType.filter(item => !this.filterCriteria.includes(item.name));
     })
   }
+
   leaveForm() {
     this.fbLeave = this.formbuilder.group({
       employeeLeaveId: [null],
@@ -74,11 +76,15 @@ export class LeaveDialogComponent {
   }
 
   onSubmit() {
+    this.fbLeave.get('fromDate').setValue(FORMAT_DATE(new Date(this.fbLeave.get('fromDate').value)));
+    this.fbLeave.get('toDate').setValue(this.fbLeave.get('toDate').value ? FORMAT_DATE(new Date(this.fbLeave.get('toDate').value)) : null);
     if (this.fbLeave.valid) {
-      this.save().subscribe(resp => { })
-      console.log(this.fbLeave.value);
-      this.ref.close(true);
-      this.alertMessage.displayAlertMessage(ALERT_CODES["ELD001"]);
+      this.save().subscribe(resp => {
+        if (resp) {
+          this.ref.close(true);
+          this.alertMessage.displayAlertMessage(ALERT_CODES["ELD001"]);
+        }
+      });
     }
     else {
       this.fbLeave.markAllAsTouched();
