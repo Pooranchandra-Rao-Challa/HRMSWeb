dad26dd47adb039ee7685572d27a04ccb681d9a8
Hima Undi <himamadhuri.u@calibrage.in>
2023-11-14 17:22:11 +0530

    Creating new components for the Applicant and Applicant Status and UI for Applicant.

---
+++ src/app/admin/applicantstatus/applicantstatus.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-applicantstatus',
+  templateUrl: './applicantstatus.component.html',
+  styles: [
+  ]
+})
+export class ApplicantstatusComponent {
+
+}
