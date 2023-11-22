8c5e449d776dbd532a79950274ffd13883e47bf5
Hima Undi <himamadhuri.u@calibrage.in>
2023-11-22 18:08:13 +0530

    Adding Upload Photo Functionality in the Add Applicant Form and Modifications in the Lookup

--- src/app/_dialogs/lookup.dialog/lookup.dialog.component.ts
+++ src/app/_dialogs/lookup.dialog/lookup.dialog.component.ts
@@ -91,8 +91,6 @@ export class LookupDialogComponent {
 
   setDependentLookup() {
     let value = this.FormControls['fkeySelfId'].value;
-    this.lookupName = ' ';
-    this.dependentLookupData = [];
     this.dependentDropdown = value > 0;
     this.getDependentLookupData(value);
   }
@@ -137,13 +135,13 @@ export class LookupDialogComponent {
     const target = event.target as HTMLInputElement;
     // Prevent the first key from being a space
     if (event.key === ' ' && (<HTMLInputElement>event.target).selectionStart === 0)
-        event.preventDefault();
+      event.preventDefault();
 
     // Restrict multiple spaces
     if (event.key === ' ' && target.selectionStart > 0 && target.value.charAt(target.selectionStart - 1) === ' ') {
-        event.preventDefault();
+      event.preventDefault();
     }
-}
+  }
 
   addLookupDetails() {
     this.ShowlookupDetails = true;
@@ -187,6 +185,9 @@ export class LookupDialogComponent {
     this.fblookup.patchValue(lookup);
     this.addFlag = false;
     this.ShowlookupDetails = true;
+    setTimeout(() => {
+      this.setDependentLookup();
+    }, 100);
   }
 
   onSubmit() {
