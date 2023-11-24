0accbf7f3a33ddcac4341d5b3c64b9bfe3ca00bc
ramya5432 <122013931+ramya5432@users.noreply.github.com>
2023-11-24 11:13:45 +0530

    changes  viewapplicant dto creation

--- src/app/_models/recruitment.ts
+++ src/app/_models/recruitment.ts
@@ -1,11 +1,81 @@
-export class ApplicantDto{
-    applicantId?:number;
-    name?:string;
-    gender?:string;
-    emailId?:string;
-    mobileNo ?:number;
-    photo?:string;
-    experienceStatus ?:string;
-    pendingDetails ?:string;
-    skills?:string;
+export class ApplicantDto {
+    applicantId?: number;
+    name?: string;
+    gender?: string;
+    emailId?: string;
+    mobileNo?: number;
+    photo?: string;
+    experienceStatus?: string;
+    pendingDetails?: string;
+    skills?: string;
+}
+export class ApplicantViewDto {
+    applicantId?: number;
+    name?: string;
+    emailId?: string;
+    dob?: string;
+    gender?: string;
+    isFresher?: string;
+    mobileNo?: number;
+    photo?: string;
+    city?: string;
+    state?: string;
+    stateId?: number;
+    country?: string;
+    countryId?: number;
+    zipCode?: number;
+    landmark?: string;
+    resumeUrl?: string;
+    applicantCertifications?: applicantCertifications;
+    applicantEducationDetails?: applicantEducationDetails;
+    applicantLanguageSkills?: applicantLanguageSkills;
+    applicantSkills?: applicantSkills;
+    applicantWorkExperience?: applicantWorkExperience;
+}
+
+export class applicantCertifications {
+    applicantId?: number;
+    applicantCertificateId?: number;
+    certificateId?: number;
+    certificateName?: string;
+    franchiseName?: string;
+    yearOfCompletion?: string;
+    results?: string;
+}
+
+export class applicantEducationDetails {
+    applicantId?: number;
+    applicantEducationDetailId?: number;
+    StreamId?: number;
+    stateId?: number;
+    state?: string;
+    countryId?: number;
+    country?: string;
+    institutionName?: string;
+    authorityName?: string;
+    yearOfCompletion?: Date;
+    gradingMethodId?: number;
+    gradingMethod?: string;
+    gradingValue?: number
+}
+export class applicantLanguageSkills {
+    applicantId?: number;
+    applicantLanguageSkillId?: number;
+    languageId?: number;
+    language?: string;
+    canRead?: boolean;
+    canSpeak?: boolean;
+    canWrite?: boolean;
+}
+
+export class applicantSkills {
+    applicantId?: number;
+    applicantSkillId?: number;
+    skillId?: number;
+    skill?: string;
+    expertise?: number;
+}
+
+export class applicantWorkExperience {
+
 }
--- src/app/admin/viewapplicant/viewapplicant.component.ts
+++ src/app/admin/viewapplicant/viewapplicant.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { DialogService, DynamicDialogRef } from 'primeng/dynamicdialog';
 import { ViewapplicantDialogComponent } from 'src/app/_dialogs/viewapplicant.dialog/viewapplicant.dialog.component';
 import { Actions, DialogRequest, ViewApplicationScreen } from 'src/app/_models/common';
@@ -20,61 +21,66 @@ export class ViewapplicantComponent {
 
   viewApplicantDialogDetails = ViewapplicantDialogComponent
   constructor(private RecruitmentService: RecruitmentService,
+    private activatedRoute: ActivatedRoute,
     public ref: DynamicDialogRef,
-    private dialogService: DialogService,) { }
+    private dialogService: DialogService,) {
+      this.applicantId = this.activatedRoute.snapshot.queryParams['applicantId'];
+     }
 
   ngOnInit() {
     // this.initviewapplicantdetails();
     this.initViewApplicantDetails();
   }
 
-  // initviewapplicantdetails(){
-  //      this.RecruitmentService.GetviewapplicantDtls(this.applicantId).subscribe((resp) => {
-  //       this.viewApplicantDetails = resp as unknown as any[];
-  //      })
-  // }
-
-  initViewApplicantDetails() {
-    // Sample data similar to the structure expected from your API
-    this.viewApplicantDetails = {
-      name: 'John Doe',
-      Male: 'Male',
-      gmail: 'john.doe@example.com',
-      phonenumber: '123-456-7890',
-      dob: '1990-01-01',
-      
-      Address: '123 Main St, City, Country',
-      Country: 'Country Name',
-      technicalSkills: [
-        { name: 'Skill 1', expertise: 4 },
-        { name: 'Skill 2', expertise: 3 },
-        // Add more skills as needed
-      ],
-      languageSkills: [
-        { name: 'English', read: true, write: true, speak: true },
-        { name: 'Spanish', read: false, write: true, speak: false },
-        // Add more languages as needed
-      ],
-      educationDetails: [
-        { institution: 'University 1', degree: 'Degree 1', duration: '2010-2014' },
-        { institution: 'University 2', degree: 'Degree 2', duration: '2015-2018' },
-        { institution: 'University 3', degree: 'Degree 3', duration: '2010-2014' },
-        { institution: 'University 4', degree: 'Degree 4', duration: '2015-2018' },
-      ],
-      experienceDetails: [
-        { company: 'Company 1', position: 'Position 1', duration: '2018-2020' },
-        { company: 'Company 2', position: 'Position 2', duration: '2021-Present' },
-        { company: 'Company 3', position: 'Position 3', duration: '2018-2020' },
-        { company: 'Company 4', position: 'Position 4', duration: '2021-Present' },
-      ],
-      certificationDetails: [
-        { name: 'Certification 1', institute: 'Institute 1', yearOfCompletion: '2019', result: 'A' },
-        { name: 'Certification 2', institute: 'Institute 2', yearOfCompletion: '2022', result: 'B' },
-        { name: 'Certification 3', institute: 'Institute 3', yearOfCompletion: '2019', result: 'A++' },
-        { name: 'Certification 4', institute: 'Institute 4', yearOfCompletion: '2022', result: 'O' },
-      ]
-    };
+  initViewApplicantDetails(){
+       this.RecruitmentService.GetviewapplicantDtls(this.applicantId).subscribe((resp) => {
+        this.viewApplicantDetails = resp as unknown as any[];
+        console.log( this.viewApplicantDetails);
+        
+       })
   }
+
+  // initViewApplicantDetails() {
+  //   // Sample data similar to the structure expected from your API
+  //   this.viewApplicantDetails = {
+  //     name: 'John Doe',
+  //     Male: 'Male',
+  //     gmail: 'john.doe@example.com',
+  //     phonenumber: '123-456-7890',
+  //     dob: '1990-01-01',
+      
+  //     Address: '123 Main St, City, Country',
+  //     Country: 'Country Name',
+  //     technicalSkills: [
+  //       { name: 'Skill 1', expertise: 4 },
+  //       { name: 'Skill 2', expertise: 3 },
+  //       // Add more skills as needed
+  //     ],
+  //     languageSkills: [
+  //       { name: 'English', read: true, write: true, speak: true },
+  //       { name: 'Spanish', read: false, write: true, speak: false },
+  //       // Add more languages as needed
+  //     ],
+  //     educationDetails: [
+  //       { institution: 'University 1', degree: 'Degree 1', duration: '2010-2014' },
+  //       { institution: 'University 2', degree: 'Degree 2', duration: '2015-2018' },
+  //       { institution: 'University 3', degree: 'Degree 3', duration: '2010-2014' },
+  //       { institution: 'University 4', degree: 'Degree 4', duration: '2015-2018' },
+  //     ],
+  //     experienceDetails: [
+  //       { company: 'Company 1', position: 'Position 1', duration: '2018-2020' },
+  //       { company: 'Company 2', position: 'Position 2', duration: '2021-Present' },
+  //       { company: 'Company 3', position: 'Position 3', duration: '2018-2020' },
+  //       { company: 'Company 4', position: 'Position 4', duration: '2021-Present' },
+  //     ],
+  //     certificationDetails: [
+  //       { name: 'Certification 1', institute: 'Institute 1', yearOfCompletion: '2019', result: 'A' },
+  //       { name: 'Certification 2', institute: 'Institute 2', yearOfCompletion: '2022', result: 'B' },
+  //       { name: 'Certification 3', institute: 'Institute 3', yearOfCompletion: '2019', result: 'A++' },
+  //       { name: 'Certification 4', institute: 'Institute 4', yearOfCompletion: '2022', result: 'O' },
+  //     ]
+  //   };
+  // }
 
   // openEditDialog(data: any): void {
   //   const ref = this.ref.open(ViewapplicantDialogComponent, {
