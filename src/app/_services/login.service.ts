a00c51cf86bf8a307228ab86b3f2f23f2b2a5670
ramya5432 <122013931+ramya5432@users.noreply.github.com>
2023-12-28 18:39:50 +0530

    changes related to refresh popup

--- src/app/_services/login.service.ts
+++ src/app/_services/login.service.ts
@@ -18,26 +18,14 @@ export class LogInSuccessModel {
     providedIn: 'root'
 })
 export class LoginService extends ApiHttpService {
-    //private respSubject?: BehaviorSubject<ResponseModel>;
     ApiUrl: string = environment.ApiUrl;
-    private userSubject: BehaviorSubject<any>;
-    public user: Observable<any>;
     private refreshTokenTimer;
 
-    public get userValue(): any {
-        if (this.userSubject != undefined)
-            return this.userSubject.getValue();
-        else return undefined;
-    }
-
     public Authenticate(data: LoginModel): Observable<LogInSuccessModel> {
-
         return this.post<ResponseModel>(LOGIN_URI, data).pipe(
             switchMap(resp => {
                 this.saveToken((resp as ResponseModel))
                 this.UpdateLookups(true);
-                //localStorage.setItem("respModel", JSON.stringify(resp as ResponseModel))
-                //this.respSubject = new BehaviorSubject<ResponseModel>(resp as ResponseModel);
                 const model: LogInSuccessModel = {
                     isFirstTimeLogin: this.IsFirstTimeLogin,
                     isLoginSuccess: true,
@@ -58,63 +46,50 @@ export class LoginService extends ApiHttpService {
                         refreshToken: u.refreshToken
                     }
                     this.jwtService.SaveToken(tokens);
-                    //   this.userSubject = new BehaviorSubject<any>(this.userValue);
-                    //   localStorage.setItem('user', JSON.stringify(this.userValue));
                     this.startRefreshTokenTimer();
                 }
             })
     }
 
-    RefreshToken(data: ResponseModel): Observable<boolean> {
-        return this.getWithParams<any>(REFRESH_TOKEN_URI, [encodeURIComponent(data.refreshToken)]).pipe(
-            switchMap(resp => {
-                this.saveToken((resp as ResponseModel))
-                return of<boolean>(true)
-            }),
-        )
-    }
-
     // resetSessionMonitor;
     openRefeshDialog() {
         let timerInterval
-         this.UserIp().subscribe(resp => {
-            Swal.fire({
-                title: 'Do you want extend the session?',
-                html: 'Session will close in <b></b> seconds.',
-                // showDenyButton: true,
-                showCancelButton: false,
-                confirmButtonColor:'#FF810E',
-                confirmButtonText: 'Refresh Session',
-                denyButtonText: `Revoke Session`,
-                timer: 60000,
-                timerProgressBar: true,
-                allowOutsideClick: false,
-                didOpen: () => {
-                    Swal.showLoading(Swal.getDenyButton())
-                    const b = Swal.getHtmlContainer().querySelector('b')
-                    timerInterval = setInterval(() => {
-                        b.textContent = Math.floor(Swal.getTimerLeft() / 2000) + ''
-                    }, 100)
-                },
-                willClose: () => {
-                    clearInterval(timerInterval);
-                },
-                customClass: {
-                    confirmButton: 'confirm-refresh-session',
-                    container: 'swal2-container-high-zindex',
-                    popup:'swal-background',
+        Swal.fire({
+            title: 'Do you want extend the session?',
+            html: 'Session will close in <b></b> seconds.',
+            // showDenyButton: true,
+            showCancelButton: false,
+            confirmButtonColor: '#FF810E',
+            confirmButtonText: 'Refresh Session',
+            denyButtonText: `Revoke Session`,
+            timer: 60000,
+            timerProgressBar: true,
+            allowOutsideClick: false,
+            didOpen: () => {
+                Swal.showLoading(Swal.getDenyButton())
+                const b = Swal.getHtmlContainer().querySelector('b')
+                timerInterval = setInterval(() => {
+                    b.textContent = Math.floor(Swal.getTimerLeft() / 2000) + ''
+                }, 100)
+            },
+            willClose: () => {
+                clearInterval(timerInterval);
+            },
+            customClass: {
+                confirmButton: 'confirm-refresh-session',
+                container: 'swal2-container-high-zindex',
+                popup: 'swal-background',
+            }
+        }).then((result) => {
+            /* Read more about isConfirmed, isDenied below */
+            if (result.isConfirmed) {
+                clearInterval(timerInterval);
+                this.refreshToken();
+            } else
+                if (result.dismiss === Swal.DismissReason.timer) {
+                    this.revokeToken(ALERT_CODES["HRMS002"]);
                 }
-            }).then((result) => {
-                /* Read more about isConfirmed, isDenied below */
-                if (result.isConfirmed) {
-                    clearInterval(timerInterval);
-                    this.refreshToken();
-                } else
-                    if (result.dismiss === Swal.DismissReason.timer) {
-                        this.revokeToken(ALERT_CODES["HRMS002"]);
-                    }
-            })
-        });
+        })
     }
 
     revokeToken(error: any = '') {
@@ -147,10 +122,6 @@ export class LoginService extends ApiHttpService {
             );
     }
 
-    logout(error: any = '') {
-        this.revokeToken(error);
-    }
-
     isLoggedIn() {
         if (!this.jwtService.JWTToken) return false;
         const jwtToken = this.jwtService.JWTToken as unknown as any;
@@ -167,14 +138,12 @@ export class LoginService extends ApiHttpService {
 
     public startRefreshTokenTimer() {
         const jwtToken = jwtdecode(this.jwtService.JWTToken) as unknown as any;
-        console.log(jwtToken);
         console.log(this.jwtService.Permissions);
         const expires = new Date(jwtToken.exp * 1000);
         console.log('Session Timeout: ' + expires)
         const timeout = expires.getTime() - (new Date()).getTime() - 60000;
         if (this.refreshTokenTimer) this.clearTimer();
         this.refreshTokenTimer = setTimeout(() => this.openRefeshDialog(), timeout);
-        // this.isLoggedIn();
     }
 
     public clearTimer() {
