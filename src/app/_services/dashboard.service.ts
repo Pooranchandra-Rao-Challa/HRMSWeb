64824c1aee17c207cc163c20edca94f6a3688b32
ramya5432 <122013931+ramya5432@users.noreply.github.com>
2023-12-29 15:44:07 +0530

    changes related to admindashboard

--- src/app/_services/dashboard.service.ts
+++ src/app/_services/dashboard.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { ApiHttpService } from './api.http.service';
-import { SelfEmployeeDto, selfEmployeeMonthlyLeaves } from '../_models/dashboard';
-import { GET_SELF_EMPLOYEE, GET_SELF_EMPLOYEE_MONTH_LEAVES } from './api.uri.service';
+import { adminDashboardViewDto, SelfEmployeeDto, selfEmployeeMonthlyLeaves } from '../_models/dashboard';
+import { GET_ADMIN_DASHBOARD, GET_SELF_EMPLOYEE, GET_SELF_EMPLOYEE_MONTH_LEAVES } from './api.uri.service';
 
 @Injectable({
     providedIn: 'root'
@@ -9,9 +9,12 @@ import { GET_SELF_EMPLOYEE, GET_SELF_EMPLOYEE_MONTH_LEAVES } from './api.uri.ser
 export class DashboardService extends ApiHttpService {
 
     public GetEmployeeDetails(employeeId: number) {
-        return this.getWithId<SelfEmployeeDto>(GET_SELF_EMPLOYEE,[employeeId])
-      }
-    public GetEmployeeLeavesForMonth(month:number,empId:number)  {
-        return this.get<selfEmployeeMonthlyLeaves>(GET_SELF_EMPLOYEE_MONTH_LEAVES+"/"+month+"/"+empId);
+        return this.getWithId<SelfEmployeeDto>(GET_SELF_EMPLOYEE, [employeeId])
+    }
+    public GetEmployeeLeavesForMonth(month: number, empId: number) {
+        return this.get<selfEmployeeMonthlyLeaves>(GET_SELF_EMPLOYEE_MONTH_LEAVES + "/" + month + "/" + empId);
+    }
+    public getAdminDashboard() {
+        return this.get<adminDashboardViewDto>(GET_ADMIN_DASHBOARD);
     }
 }
