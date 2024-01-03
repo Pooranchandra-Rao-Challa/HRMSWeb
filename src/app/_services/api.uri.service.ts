bee41e0c06c4a83ad0e89c5460a06cfa0c3bb0a8
Hima Undi <himamadhuri.u@calibrage.in>
2024-01-03 19:37:50 +0530

    changes related to the Holiday Configuration

--- src/app/_services/api.uri.service.ts
+++ src/app/_services/api.uri.service.ts
@@ -164,6 +164,6 @@ export const GET_ADMIN_DASHBOARD="AdminDashboard/GetAdminDashboard"
 //Reports
 export const GET_MONTHLY_ATTENDANCE_REPORT="Report/MonthlyAttendanceReport";
 export const GET_PROJECTS_REPORT="Report/ProjectDetailsReport";
-export const GET_HOLIDAYS_REPORT="";
+export const GET_HOLIDAYS_REPORT="Report/HolidaysReport";
 export const GET_LEAVES_REPORT="Report/LeaveStatisticsReport";
 export const GET_ASSETS_REPORT="Report/AssetsReport";
