4cc6cb701d850c768511c570f3ec7617da2258b4
ramya5432 <122013931+ramya5432@users.noreply.github.com>
2024-01-03 18:03:23 +0530

    changes related to  assets

--- src/app/_services/report.service.ts
+++ src/app/_services/report.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { ApiHttpService } from './api.http.service';
-import { GET_MONTHLY_ATTENDANCE_REPORT } from './api.uri.service';
+import { GET_ASSETS_REPORT, GET_MONTHLY_ATTENDANCE_REPORT } from './api.uri.service';
 
 @Injectable({
   providedIn: 'root'
@@ -9,5 +9,9 @@ export class ReportService  extends ApiHttpService{
 
   DownloadMonthlyAttendanceReport(month: number,year:number){
    return this.downloadExcel(GET_MONTHLY_ATTENDANCE_REPORT,[month,year])
+  }
+
+  DownloadAssets(assetsStatus:number){
+    return this.downloadExcel(GET_ASSETS_REPORT,[assetsStatus])
   }
 }
