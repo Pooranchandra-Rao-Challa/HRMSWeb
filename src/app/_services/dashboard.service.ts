07ebc1c0e0e1b3a1564a3295d6ee369c0fefcf58
Madhuri-Minna <madhuri.minna@calibrage.in>
2024-01-04 19:23:07 +0530

    Attendance screen adding PL's and CL's

--- src/app/_services/dashboard.service.ts
+++ src/app/_services/dashboard.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { ApiHttpService } from './api.http.service';
 import { adminDashboardViewDto, SelfEmployeeDto, selfEmployeeMonthlyLeaves } from '../_models/dashboard';
-import { GET_ADMIN_DASHBOARD, GET_SELF_EMPLOYEE, GET_SELF_EMPLOYEE_MONTH_LEAVES } from './api.uri.service';
+import { GET_ADMIN_DASHBOARD, GET_ALLOTED_LEAVES, GET_SELF_EMPLOYEE, GET_SELF_EMPLOYEE_MONTH_LEAVES } from './api.uri.service';
 
 @Injectable({
     providedIn: 'root'
@@ -10,6 +10,9 @@ export class DashboardService extends ApiHttpService {
 
     public GetEmployeeDetails(employeeId: number) {
         return this.getWithId<SelfEmployeeDto>(GET_SELF_EMPLOYEE, [employeeId])
+    }
+    public GetAllottedLeavesBasedOnEId(employeeId:number,month:number,year:number){
+        return this.getWithParams(GET_ALLOTED_LEAVES,[employeeId,month,year]);
     }
     public GetEmployeeLeavesForMonth(month: number, empId: number,year:number) {
         return this.getWithParams<selfEmployeeMonthlyLeaves>(GET_SELF_EMPLOYEE_MONTH_LEAVES ,[month ,empId,year]);
