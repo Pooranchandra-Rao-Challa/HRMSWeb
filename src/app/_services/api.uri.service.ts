728c9ab5298dfe29329f0c84b71d6830c751acb6
pcraochallas@calibrage.in <pcraochallas@calibrage.in>
2024-01-10 09:28:08 +0530

    Updated for Projects

--- src/app/_services/api.uri.service.ts
+++ src/app/_services/api.uri.service.ts
@@ -72,6 +72,7 @@ export const GET_EMPLOYEESLIST = "Employee/GetEmployeesForProject";
 export const UNASSIGNED_EMPLOYEE_URI = "Admin/InactiveProjectAllotment";
 export const GET_EMPLOYEE_HIERARCHY_BASED_ON_PROJECTS = "Admin/GetEmployeeHierarchy";
 export const GET_EMPLOYEE_ROLES_INFO = "Admin/GetERoles";
+export const GET_PROJECT_LOGO = "Admin/GetProjectLogo";
 
 //EMPLOYEE
 export const GET_EMPLOYEES_URI = "Employee/GetEmployeeDetails";
