f7bd9a45775f8bb45defebc1520b34b387d7393e
Hima Undi <himamadhuri.u@calibrage.in>
2024-01-18 12:36:24 +0530

    changes related to the admin dashboard

--- src/app/_models/dashboard.ts
+++ src/app/_models/dashboard.ts
@@ -116,6 +116,7 @@ export class EmployeesofAttendanceCountsViewDto{
     dayWorkStatus?:string;
     employeeName?:string;
     code?:string;
+    monthNames?:string;
 }
 
 export class activeProjects {
